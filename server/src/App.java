f91081a42c3f6339c341d685c31b7e83346b8285
Hugo <hugorezende96@hotmail.com>
2020-10-06 12:22:20 -0300

    servidor POST/GET

--- server/src/App.java
+++ server/src/App.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,20 +10,42 @@ public class App {
         ServerSocket server = new ServerSocket(5555);
 
         System.out.println("Esperando Conexão...");
-        //Esperar conexão...
+        // Esperar conexão...
         Socket connection = server.accept();
 
         System.out.println("Conexão chegou!");
 
-        int nBytes = connection.getInputStream().read(); 
+        // Pega os dados que estão chegando
+        InputStream inBytes = connection.getInputStream();
 
-        String response = "HTTP/1.1 200 OK\n"
-            + "Content-Type: text/html\n\n"
-            + "<h1>Bytes Recebidos: " + nBytes +"</h1>";
+        InputStreamReader leitorBytes = new InputStreamReader(inBytes);
+
+        //Colocar os bytes em um objeto que entenda eles
+        BufferedReader leitorString = new BufferedReader(leitorBytes);
+
+        String request = "";
+
+        while(true){
+            String line = leitorString.readLine();
             
-        byte[] bytesResponce = response.getBytes();
+            if (line.isEmpty()){
+                break;
+            }
+            
+            request += line + "\n";
+        }
 
-        connection.getOutputStream().write(bytesResponce);
+        System.out.println(request);
+
+        String responseHttp = "HTTP/1.1 200 OK\n"
+            + "Access-Control-Allow-Origin: *\n"
+            + "Content-Type: text/html\n\n"
+            + request;
+
+
+        byte[] bytesResponse = responseHttp.getBytes();
+
+        connection.getOutputStream().write(bytesResponse);
 
         System.out.println("Fechando Servidor...");
         server.close();
